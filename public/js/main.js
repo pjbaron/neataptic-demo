7b37c665c2bddcac7b8bbcb013b41be90f67de31
Pete Baron <sibaroni@hotmail.com>
2021-01-31 16:11:45 +1300

    modified neatptic-tron demo

---
+++ public/js/main.js
@@ -0,0 +1,184 @@
+/** Rename vars */
+var Neat    = neataptic.Neat;
+var Methods = neataptic.Methods;
+var Config  = neataptic.Config;
+var Architect = neataptic.Architect;
+
+/** Turn off warnings */
+Config.warnings = false;
+
+/** Settings */
+var SPEED 			 = 8;
+var WIDTH            = 1024;	//$(window).width();
+var HEIGHT           = 768; 	//$(window).height();
+var MAX_SPEED        = WIDTH/150;
+var START_X          = WIDTH/2;
+var START_Y          = HEIGHT/2;
+var SCORE_RADIUS     = WIDTH/6;
+
+// GA settings
+var PLAYER_AMOUNT    = 200;
+var ITERATIONS       = 200;
+var MUTATION_RATE    = 0.2;
+var MAX_MUTATION	 = 0.5;
+var ELITISM          = Math.round(0.25 * PLAYER_AMOUNT);
+
+// Trained population
+var USE_TRAINED_POP = false;
+
+//track when we won...
+var firstConvergence = -1
+
+/** Global vars */
+var neat;
+var players = [];
+
+
+/** Construct the genetic algorithm */
+function initNeat(){
+  neat = new Neat(
+    6, 2,
+    null,
+    {
+      mutation: [
+        Methods.Mutation.ADD_NODE,
+        Methods.Mutation.ADD_NODE,
+        Methods.Mutation.SUB_NODE,
+        Methods.Mutation.ADD_CONN,
+        Methods.Mutation.ADD_CONN,
+        Methods.Mutation.SUB_CONN,
+        Methods.Mutation.ADD_GATE,
+        Methods.Mutation.ADD_GATE,
+        Methods.Mutation.SUB_GATE,
+        Methods.Mutation.MOD_WEIGHT,
+        Methods.Mutation.MOD_WEIGHT,
+        Methods.Mutation.MOD_BIAS,
+        Methods.Mutation.MOD_BIAS,
+        Methods.Mutation.MOD_ACTIVATION,
+        Methods.Mutation.MOD_ACTIVATION,
+        //Methods.Mutation.ADD_SELF_CONN,
+        //Methods.Mutation.SUB_SELF_CONN,
+        //Methods.Mutation.ADD_BACK_CONN,
+        //Methods.Mutation.SUB_BACK_CONN
+      ],
+      popsize: PLAYER_AMOUNT,
+      mutationRate: MAX_MUTATION,
+      elitism: ELITISM
+    }
+  );
+
+  if(USE_TRAINED_POP){
+    neat.population = population;
+  }
+  
+  // Draw the first graph
+  drawGraph(neat.population[0].graph($('.best').width()/2, $('.best').height()/2), '.best');
+}
+
+
+/** Start the evaluation of the current generation */
+function startEvaluation(){
+  //console.log('population: ' + neat.population.length)
+  highestScore = 0;
+
+  //for(var genome in neat.population){
+  for(var v=0;v<neat.population.length;v++)
+  {
+    genome = neat.population[v];
+    if(!players[v])
+    {
+      players[v] = new Player(genome, v);
+    }
+    else
+    {
+      // create a new player at this old player's location
+      var oldPos = players[v].getPos()
+      var oldScore = players[v].getScore()
+      players[v] = new Player(genome, v);
+
+      // fix NaN
+      oldPos.x = isNaN(oldPos.x) ? 0 : oldPos.x
+      oldPos.y = isNaN(oldPos.y) ? 0 : oldPos.y
+      oldPos.ax = isNaN(oldPos.ax) ? 0 : oldPos.ax
+      oldPos.ay = isNaN(oldPos.ay) ? 0 : oldPos.ay
+      oldPos.vx = isNaN(oldPos.vx) ? 0 : oldPos.vx
+      oldPos.vy = isNaN(oldPos.vy) ? 0 : oldPos.vy
+
+      players[v].setPos(oldPos)
+      players[v].setScore(0)	//(oldScore * .5)
+    }
+  }
+
+  walker.reset();
+}
+
+
+
+/** End the evaluation of the current generation */
+function endEvaluation()
+{
+  var totalScore = 0
+  var averageScore = 0
+
+  // Sort the population by score
+  neat.sort();
+
+  // Draw the best genome
+  drawGraph(neat.population[0].graph($('.best').width()/2, $('.best').height()/2), '.best');
+
+  // Find average score of the elite group
+  for(var i = 0; i < neat.elitism; i++)
+  {
+  	var j = neat.population[i].genomeIdx;
+    totalScore += Math.max(players[j].brain.score, 0);
+  }
+  averageScore = totalScore / neat.elitism;
+
+  // Variable mutation... (Lerp towards target)
+  let mutationTarget = MUTATION_RATE + (1 - Math.min(averageScore / 150, 1)) * (MAX_MUTATION - MUTATION_RATE);
+  neat.mutationRate += (mutationTarget - neat.mutationRate) * 0.1;
+  neat.mutationRate = Math.min(neat.mutationRate, MAX_MUTATION);
+
+  if (firstConvergence == -1 && averageScore > 100)
+  {
+    if(neat.generation > 10)
+    {
+      firstConvergence = neat.generation
+      SPEED = 1;
+      $('#firstConvergence').html(firstConvergence)
+    }
+  }
+
+
+  // Init new pop
+  var newPopulation = [];
+
+  // Breed the next individuals with Elitism for the best of the last generation
+  for(var i = 0; i < neat.popsize; i++)
+  {
+  	if (i < neat.elitism)
+    	newPopulation.push(neat.population[i]);
+    else
+	    newPopulation.push(neat.getOffspring());
+  }
+
+  // Replace the old population with the new population
+  neat.population = newPopulation;
+
+  // Mutate the genomes
+  neat.mutate();
+
+  // Networks shouldn't get too big (ignore input and output size, they're fixed so should not penalise!)
+  for(var g in neat.population)
+  {
+    let genome = neat.population[g];
+    genome.score -= (genome.nodes.length - (genome.input + genome.output)) * 0.1;
+  }
+
+  neat.generation++;
+  $('#generation').html(neat.generation)
+  $('#mutationRate').html(Math.floor(neat.mutationRate * 1000) / 1000);
+  $('#averageScore').html(Math.floor(averageScore * 1000) / 1000);
+
+  startEvaluation();
+}
---
+++ public/js/player.js
@@ -0,0 +1,125 @@
+function Player(genome, v){
+  this.x = Math.random() * WIDTH;
+  this.y = Math.random() * HEIGHT;
+
+  this.vx = 0;
+  this.vy = 0;
+  this.r = 6;
+
+  this.brain = genome;
+  this.brain.genomeIdx = v;
+  this.brain.score = 0;
+}
+
+
+Player.prototype = {
+  /** Update the stats */
+  update: function(){
+    var input = this.detect();
+    var output = this.brain.activate(input);
+    var moveangle = output[0] * 2 * PI;
+
+    // Calculate next position
+    this.ax = Math.cos(moveangle)
+    this.ay = Math.sin(moveangle)
+
+    // Apply only a fraction of the angular velocity to simulate inertia
+    this.vx += this.ax * .8 * output[1]
+    this.vy += this.ay * .8 * output[1]
+
+    // Limit speeds to maximum speed
+    this.vx = this.vx > MAX_SPEED ? MAX_SPEED : this.vx < -MAX_SPEED ? -MAX_SPEED : this.vx;
+    this.vy = this.vy > MAX_SPEED ? MAX_SPEED : this.vy < -MAX_SPEED ? -MAX_SPEED : this.vy;
+
+    // Friction
+    // this.vx -= (this.vx * 0.01);
+    // this.vy -= (this.vy * 0.01);
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    // Limit position to width and height
+    //this.x = this.x >= WIDTH  ? WIDTH  : this.x <= 0 ? 0 : this.x;
+    //this.y = this.y >= HEIGHT ? HEIGHT : this.y <= 0 ? 0 : this.y;
+    this.x = this.x >= WIDTH  ? (this.x - WIDTH)  : this.x <= 0 ? (WIDTH - this.x) : this.x;
+    this.y = this.y >= HEIGHT  ? (this.y - HEIGHT)  : this.y <= 0 ? (HEIGHT - this.y) : this.y;
+
+    if(this.x == 0 || this.x == WIDTH) this.vx = -this.vx;
+    if(this.y == 0 || this.y == HEIGHT) this.vy = -this.vy;
+
+    this.score();
+  },
+  exit: function(){
+    return;
+  },
+  getPos: function(){
+    return({"x": this.x, "y": this.y, "vx" : this.vx, "vy":this.vy})
+  },
+  setPos: function(newPos){
+    this.x = newPos.x
+    this.y = newPos.y
+    this.vx = newPos.vx
+    this.vy = newPos.vy
+  },
+  getScore: function(){
+    return this.brain.score
+  },
+  setScore: function(newScore){
+    this.brain.score = newScore
+  },
+
+  /** Calculate fitness of this players genome **/
+  score: function(){
+    var dist = distance(this.x, this.y, walker.x, walker.y);
+    if(!isNaN(dist) && dist < SCORE_RADIUS){
+      this.brain.score += (SCORE_RADIUS - dist) / SCORE_RADIUS;
+      if(this.brain.score <= 0) this.brain.score = 0
+    }
+
+    // Replace highest score to visualise
+    highestScore = this.brain.score > highestScore ? this.brain.score : highestScore;
+  },
+
+  /** Display the player on the field, parts borrowed from the CodingTrain */
+  show: function(){
+    // Draw a triangle rotated in the direction of velocity
+    var angle = Math.atan2(this.vy, this.vx) + Math.PI / 2.0;
+    var color = activationColor(this.brain.score, highestScore);
+
+    push();
+    translate(this.x, this.y);
+    rotate(angle);
+
+    stroke(color);
+    //console.log(color)
+    fill([0,0,0]);
+    beginShape();
+    vertex(0, -this.r * 2);
+    vertex(-this.r, this.r * 2);
+    vertex(this.r, this.r * 2);
+    endShape(CLOSE);
+
+    pop();
+  },
+
+  /** Detect and normalize inputs */
+  detect: function(){
+    var dx = walker.x - this.x;
+    var dy = walker.y - this.y;
+
+    // world is toroidal, wrap around at edges
+    if (dx > WIDTH / 2) dx = WIDTH - dx;
+    if (dx < -WIDTH / 2) dx = WIDTH + dx;
+    if (dy > HEIGHT / 2) dy = HEIGHT - dy;
+    if (dy < -HEIGHT / 2) dy = HEIGHT + dy;
+
+    var targetAngle = (Math.atan2(dy, dx) + Math.PI / 2.0) / TWO_PI;
+    var dist = Math.sqrt(dx * dx + dy * dy) / Math.sqrt(WIDTH*WIDTH + HEIGHT*HEIGHT);
+    var vx = this.vx / MAX_SPEED;
+    var vy = this.vy / MAX_SPEED;
+    var tvx = walker.vx / MAX_SPEED;
+    var tvy = walker.vy / MAX_SPEED;
+
+    return [vx, vy, tvx, tvy, targetAngle, dist];
+  },
+};
